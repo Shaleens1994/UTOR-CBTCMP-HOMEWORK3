e03a63707f2209f7da4b20eba1010e3d76a951af
shaleen shaji <shaleens1994@gmail.com>
2022-01-20 19:41:10 -0500

    Readme and other updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # UTOR-CBTCMP-HOMEWORK3
+
+Password-Generator  Webpage with details as specified in acceptance criteria has been created!
+
+
+![Password Generator](./Assets/03-javascript-homework-demo.png)
+
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let input = "default"
+let input;
 var lcasechar;
 var ucasechar;
 var ncasechar;
@@ -33,8 +33,8 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
 input=prompt("enter maximum length of charcter");
 console.log(input);
-{if(input >= 8 && input <=128)
-{alert("Accepted");
+{if(input >= 8 && input <=128) // conditional sttement to match first criteria
+{alert("Range Accepted- Click OK to Continue!");
 lcasechar = confirm("Include Lower Case");
 ucasechar = confirm("Include upper Case");
 ncasechar = confirm("Include Numerical Characters");
@@ -42,11 +42,6 @@ scasechar = confirm("Include Special Characters Case");
 finalpassword="0";
 finalpassword = writePasswordfirst()
 return finalpassword;}
-
-if (lcasechar==true)
-  {console.log("hoog");
-
-}
 
 else { alert("not possible");}
 }}
@@ -60,39 +55,8 @@ function writePasswordfirst() {
 
   for(i = 0;i<(input);i++)
   {
-    final+=charcodes[Math.floor(Math.random() * charcodes.length)];
+    final+=charcodes[Math.floor(Math.random() * charcodes.length)]; // For loop to add individual characters based on the criteria selected
   }
-  /*
-  if(lcasechar==true)
-  {
-    
-    for(i = 0;i<(input/4);i++)
-    {
-      final+=lowercase[Math.floor(Math.random() * 26)];
-    }}
-  
-    if(ucasechar==true)
-  {
-    
-    for(i;i<(input/3);i++)
-    {
-      final+=uppercase[Math.floor(Math.random() * 26)];
-    }}
-
-    if(scasechar==true)
-  {
-    
-    for(i;i<(input/2);i++)
-    {
-      final+=spchar[Math.floor(Math.random() * 26)];
-    }}
-    if(ncasechar==true)
-    {
-      
-      for(i;i<(input);i++)
-      {
-        final+=numchar[Math.floor(Math.random() * 10)];
-      }}*/
   
 
     return final;
@@ -101,47 +65,3 @@ function writePasswordfirst() {
   
 
 
-
-/*function myFunction() {
-  var txt;
-  if (confirm("Press a button!")) {
-    txt = "You pressed OK!";
-  } else {
-    txt = "You pressed Cancel!";
-  }
-  document.getElementById("demo").innerHTML = txt;
-}
-function returnText(){
-  input = document.getElementById("userInput").value;
-  result = input.includes(" !","#$%&'()*+,-./:;<=>?@[\]^_`{|}~"", 0);
-  if (result){
-    alert("Good")
-  } else{}
-  let spChar = "/[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]+/";
-  for (var i = 0; i < input.length; i++) {
-     if (spChar.indexOf(input.charAt(i)) != -1) {
-         return true;
-     }
-  }
-  return false;
-  
-}
-
-if (input="password") {
-  alert("Password Okay")
-} else { alert("not Okay")}
-
-function check(_string) {
-  let spChar = "/[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]+/";
-  for (var i = 0; i < _string.length; i++) {
-     if (spChar.indexOf(_string.charAt(i)) != -1) {
-         return true;
-     }
-  }
-  return false;
-}
-
-function containsSpecialChars(str) {
-  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  return specialChars.test(str);
-} */ 
